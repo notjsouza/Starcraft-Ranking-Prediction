a53baa6ec41740e3ca9d1b2cfdf73f253f4d0023
Justin <notjsouza@gmail.com>
2023-05-27 23:30:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,24 +4,39 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
+import matplotlib.pyplot as plt
+
 # load csv file into a pandas dataframe
 df = pd.read_csv('starcraft_player_data.csv')
 
-# removes all players containing unknown data values, '?', from the dataframe
+# remove all players containing unknown data values, '?', from the dataframe
 df = df.where(df != '?').dropna()
 df.head()
 
-# splits the dataframe into feature variable x_df and target variable y_df
-x_df = df.drop(columns=['GameID', 'LeagueIndex'])
-y_df = df['LeagueIndex']
+# creating a histogram to map player density per LeagueIndex
+plt.hist(df['LeagueIndex'])
+plt.xlabel('LeagueIndex')
+plt.ylabel('Player Density')
+plt.title('Player Density per LeagueIndex')
+plt.show()
 
-# 90/10 split training data and test data
-x_train, x_test, y_train, y_test = train_test_split(x_df, y_df, test_size=0.1, random_state=1)
+# logically we knew there wouldn't be a linear trend, but I like to be certain.
 
-# creates a model to predict the rank of the player
+# the histogram shows no linear trend for LeagueIndex, and we don't know which parameter is
+# more or less important to calculating rank, so I decided to use a DecisionTreeClassifier
+# model, which will run multiple splits and use the split with the error closest to zero.
+
+# splits the dataframe into feature variable x, last 18 columns, and target variable y, LeagueIndex
+x = df.drop(columns=['GameID', 'LeagueIndex'])
+y = df['LeagueIndex']
+
+# using a 90/10 split between training data and test data to thoroughly train the model
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=1)
+
+# creates the DecisionTreeClassifier model and fits it to the training data
 model = DecisionTreeClassifier(max_depth=8, random_state=4)
 model.fit(x_train, y_train)
 predictions = model.predict(x_test)
 
-# calculates the accuracy of the model by comparing the prediction to the recorded rank
+# calculates the accuracy of the model by comparing the predicted rank to the recorded rank
 print('Accuracy:', str(round(accuracy_score(y_test, predictions) * 100, 2)) + '%')
